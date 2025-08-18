d26af4eccfcc7c86b8a3714783c6ce5076b51a23
abhi0420 <abhiharish2000@gmail.com>
2025-08-18 23:56:03 +0530

    Commiting changes to analyse module

--- src/analyze.py
+++ src/analyze.py
@@ -1,6 +1,9 @@
 from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 import torch
 import torch.nn.functional as F
+import warnings
+
+warnings.filterwarnings("ignore")
 
 MODEL_NAME = "yangheng/deberta-v3-base-absa-v1.1"
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
@@ -16,10 +19,24 @@ def analyze_sentiment(text, aspect=None):
     outputs = model(**inputs)
     probs = F.softmax(outputs.logits, dim=-1)[0]
     labels = model.config.id2label
-    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+    max_score = max(labels, key = labels.get)
+    check_sarcasm = analyze_sarcasm(text)
+    if check_sarcasm:
+        if labels[max_score].lower() ==  "positive":
+            labels[max_score] = "negative"
+        else:
+            labels[max_score] = "positive"
+    return {labels[max_score]: float(probs[max_score])}
+
+def analyze_sarcasm(text):
+    sarcasm = sarcasm_detection_pipeline(text)[0] 
+    if sarcasm['label'] == 'irony':
+        if sarcasm['score'] > 0.8:
+            return True
+    return False
 
 if __name__ == "__main__":
-    text = "I love using transformers for NLP tasks!"
+    text = "You are great at making terrible decisions"
     aspect = "transformers"
     result = analyze_sentiment(text, aspect)
     print(result)
---
+++ src/main.py
@@ -0,0 +1,3 @@
+from fetch_data import fetch_reddit_data
+from analyze import analyze_sentiment
+
